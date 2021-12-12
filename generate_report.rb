aed25f649d3cdaa10bc526a2fe9bd31bfe91b660
Thai Pangsakulyanont <dtinth@spacet.me>
2021-12-12 15:22:30 +0000

    Add report

---
+++ generate_report.rb
@@ -0,0 +1,241 @@
+require 'json'
+
+TestResult = Struct.new(:git_revision, :version) do
+  def url
+    "https://github.com/facebook/react/tree/#{git_revision}"
+  end
+
+  def blob_url
+    "https://github.com/facebook/react/tree/#{git_revision}"
+  end
+
+  def data
+    @data ||= JSON.parse(File.read("data/react-tests-#{git_revision}-stable.json"))
+  end
+
+  def total_test_suites
+    data['numTotalTestSuites']
+  end
+
+  def total_tests
+    data['numTotalTests']
+  end
+
+  def test_names
+    @test_names ||= data['testResults'].flat_map { |r| r['assertionResults'].map { |a| a['fullName'] } }
+  end
+
+  def report_filename
+    "#{version}.md"
+  end
+
+  def suite_names
+    @suite_names ||= (data['testResults'].map { |r| r['name'] }).sort
+  end
+
+  def suite(name)
+    TestSuite.new(self, data['testResults'].find { |r| r['name'] == name })
+  end
+
+  def generate_report
+    filename = report_filename
+    File.open "reports/#{filename}", 'w' do |out|
+      out.puts "# [#{version}](#{url})"
+      out.puts
+      writer = AncestralWriter.new(out)
+      suite_names.each do |suite_name|
+        suite = suite(suite_name)
+        out.puts "- `#{suite.name}`"
+        suite.tests.each do |test|
+          writer.write test.ancestor_titles, "- #{test.link}"
+        end
+      end
+    end
+  end
+end
+
+TestSuite = Struct.new(:test_result, :data) do
+  def found?
+    !data.nil?
+  end
+
+  def name
+    return nil unless data
+    data['name'].sub('/home/runner/work/react-tests/react-tests/react/', '')
+  end
+
+  def test_names
+    return [] unless data
+    tests.map { |r| r.full_name }
+  end
+
+  def tests
+    return [] unless data
+    data['assertionResults']
+      .filter { |r| r['status'] == 'passed' }
+      .reject { |r| r['fullName']['[GATED, SHOULD FAIL]'] }
+      .map { |r| Test.new(self, r) }
+  end
+
+  def url
+    "#{test_result.blob_url}/#{name}"
+  end
+end
+
+Test = Struct.new(:test_suite, :data) do
+  def full_name
+    data['fullName']
+  end
+
+  def ancestor_titles
+    data['ancestorTitles']
+  end
+
+  def url
+    test_suite.url + "#L#{data['location']['line']}"
+  end
+
+  def link_name
+    data['title'].strip.lines.first.strip
+  end
+
+  def link
+    "[#{link_name}](#{url})"
+  end
+end
+
+test_results = [
+  TestResult.new('v17.0.0', 'v17.0.0'),
+  TestResult.new('v17.0.1', 'v17.0.1'),
+  TestResult.new('v17.0.2', 'v17.0.2'),
+  TestResult.new('e6be2d531', 'v18.0.0-alpha'),
+  TestResult.new('96ca8d915', 'v18.0.0-beta'),
+  TestResult.new('f2a59df48', 'v18.0.0-rc.0'),
+]
+
+comparisons = []
+
+Comparison = Struct.new(:current, :previous) do
+  def changes_count
+    added = current.test_names - previous.test_names
+    removed = previous.test_names - current.test_names
+    [added.size, removed.size]
+  end
+
+  def report_filename
+    return nil unless previous
+    added, removed = changes_count
+    return nil if added == 0 && removed == 0
+    "#{previous.version}_VS_#{current.version}.md"
+  end
+
+  def changes
+    return '-' unless previous
+    added, removed = changes_count
+    filename = report_filename
+    text = "+#{added}, -#{removed}"
+    filename ? "[#{text}](#{filename})" : text
+  end
+
+  def suite_names
+    @suite_names ||= (current.suite_names | previous.suite_names).sort
+  end
+
+  def comparisons
+    suite_names
+      .map { |s| SuiteComparison.new(current.suite(s), previous.suite(s)) }
+      .select(&:changed?)
+  end
+
+  def generate_report
+    filename = report_filename
+    return unless filename
+    File.open "reports/#{filename}", 'w' do |out|
+      comparisons.each do |comparison|
+        out.puts "- `#{comparison.suite_name}`"
+        writer = AncestralWriter.new(out)
+        comparison.added.each do |test|
+          writer.write test.ancestor_titles, "- (+) #{test.link}"
+        end
+        writer = AncestralWriter.new(out)
+        comparison.removed.each do |test|
+          writer.write test.ancestor_titles, "- (-) #{test.link}"
+        end
+      end
+    end
+  end
+end
+
+class AncestralWriter
+  def initialize(out)
+    @out = out
+    @current = []
+  end
+  def write(ancestor_titles, text)
+    longest_common_size = @current.zip(ancestor_titles).take_while { |a, b| a == b }.size
+    while @current.size > longest_common_size
+      @current.pop
+    end
+    while @current.size < ancestor_titles.size
+      @current << ancestor_titles[@current.size]
+      @out.puts "  " * @current.size + '- ' + @current.last
+    end
+    @out.puts "  " * (@current.size + 1) + text
+  end
+end
+
+# Comparisons of suite with same name
+SuiteComparison = Struct.new(:current, :previous) do
+  def suite_name
+    current.name || previous.name
+  end
+
+  def changed?
+    return true if !current.found?
+    return true if !previous.found?
+    added = current.test_names - previous.test_names
+    removed = previous.test_names - current.test_names
+    added.size > 0 || removed.size > 0
+  end
+
+  def added
+    previous_names = previous.test_names
+    current.tests.select { |t| !previous_names.include?(t.full_name) }
+  end
+
+  def removed
+    current_names = current.test_names
+    previous.tests.select { |t| !current_names.include?(t.full_name) }
+  end
+end
+
+File.open 'reports/README.md', 'w' do |out|
+  out.puts "| Version | Suites | Tests | Compared to<br>previous | Compared to<br>v17.0.2 |"
+  out.puts "| --- | ---:| ---:| ---:| ---:|"
+  last = nil
+  v17 = nil
+  test_results.each do |r|
+    compare_last = Comparison.new(r, last)
+    compare_v17 = Comparison.new(r, v17)
+    cols = [
+      "[#{r.version}](#{r.url})",
+      "#{r.total_test_suites}",
+      "[#{r.total_tests}](#{r.report_filename})",
+      compare_last.changes,
+      compare_v17.changes,
+    ]
+    out.puts "| #{cols * ' | '} |"
+    last = r
+    v17 = r if r.version == 'v17.0.2'
+    comparisons << compare_last
+    comparisons << compare_v17
+  end
+end
+
+comparisons.each do |c|
+  c.generate_report
+end
+
+test_results.each do |t|
+  t.generate_report
+end
---
+++ reports/README.md
@@ -0,0 +1,8 @@
+| Version | Suites | Tests | Compared to<br>previous | Compared to<br>v17.0.2 |
+| --- | ---:| ---:| ---:| ---:|
+| [v17.0.0](https://github.com/facebook/react/tree/v17.0.0) | 253 | [6870](v17.0.0.md) | - | - |
+| [v17.0.1](https://github.com/facebook/react/tree/v17.0.1) | 253 | [6870](v17.0.1.md) | +0, -0 | - |
+| [v17.0.2](https://github.com/facebook/react/tree/v17.0.2) | 253 | [6862](v17.0.2.md) | [+1, -9](v17.0.1_VS_v17.0.2.md) | - |
+| [v18.0.0-alpha](https://github.com/facebook/react/tree/e6be2d531) | 266 | [6968](v18.0.0-alpha.md) | [+656, -550](v17.0.2_VS_v18.0.0-alpha.md) | [+656, -550](v17.0.2_VS_v18.0.0-alpha.md) |
+| [v18.0.0-beta](https://github.com/facebook/react/tree/96ca8d915) | 281 | [7469](v18.0.0-beta.md) | [+737, -237](v18.0.0-alpha_VS_v18.0.0-beta.md) | [+1182, -576](v17.0.2_VS_v18.0.0-beta.md) |
+| [v18.0.0-rc.0](https://github.com/facebook/react/tree/f2a59df48) | 280 | [7492](v18.0.0-rc.0.md) | [+35, -12](v18.0.0-beta_VS_v18.0.0-rc.0.md) | [+1215, -586](v17.0.2_VS_v18.0.0-rc.0.md) |
